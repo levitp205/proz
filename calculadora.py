0b738ecc6b5e3763ae039da06819cb854ef327fd
levitp <levi.provetti@gmail.com>
2023-11-16 06:40:58 -0300

    melhoria na calculadora

--- calculadora.py
+++ calculadora.py
@@ -19,28 +19,30 @@ def calculadora():
         print("2. Subtração")
         print("3. Multiplicação")
         print("4. Divisão")
+        print("0. Sair")
+        
 
         operacao = input("Digite o número da opração desejada: ")
-
-        if operacao in ('1', '2', '3','4'):
-            num1 = int(input("Digite o primeiro número:  "))
-            num2 = int(input("Digite o segundo número: "))
-    
-            if operacao == '1':
-                soma = num1 + num2
-                print(f'Resultado: {num1} + {num2} = {soma}')
-            elif operacao == '2':
-                soma = num1 - num2
-                print(f'Resultado: {num1} - {num2} = {soma}')    
-            elif operacao == '3':
-                soma = num1 * num2
-                print(f'Resultado: {num1} * {num2} = {soma}')  
-            elif operacao == '4':
-                soma = num1 - num2
-                print(f'Resultado: {num1} / {num2} = {soma}')    
-        else:
-            print("O resultado da operção é '0'") 
+        if operacao == '0':
+            print("Saindo...") 
             break
+        elif operacao in ('1', '2', '3','4'):
+            num1 = int(input("Digite o primeiro número:  "))
+            num2 = int(input("Digite o segundo número: "))      
+        if operacao == '1':
+            soma = num1 + num2
+            print(f'Resultado: {num1} + {num2} = {soma}')
+        elif operacao == '2':
+            soma = num1 - num2
+            print(f'Resultado: {num1} - {num2} = {soma}')    
+        elif operacao == '3':
+            soma = num1 * num2
+            print(f'Resultado: {num1} * {num2} = {soma}')  
+        elif operacao == '4':
+            soma = num1 - num2
+            print(f'Resultado: {num1} / {num2} = {soma}') 
+        else:      
+            print("Esta opração não exite...tente novamente!") 
 
 cal =calculadora() 
 print(cal)     
